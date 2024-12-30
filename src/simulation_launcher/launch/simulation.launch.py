480ebb84f9b69231bcd190ad178bcb78a3ca2710
Ayrton2718 <matsushita.sena@gmail.com>
2024-12-30 15:23:55 +0900

    Add waffle lidar.

---
+++ src/simulation_launcher/launch/simulation.launch.py
@@ -0,0 +1,54 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_prefix
+import subprocess
+
+package_name = 'simulation_launcher'
+
+def generate_launch_description():
+    subprocess.run(["blackbox_create"]) 
+
+    ld = LaunchDescription()
+    x_pose = LaunchConfiguration('x_pose', default='-2.0')
+    y_pose = LaunchConfiguration('y_pose', default='-0.5')
+    
+    ld.add_action(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('aws_robomaker_small_warehouse_world'), 'launch', 'no_roof_small_warehouse.launch.py')
+            )
+        )
+    )
+    
+    ld.add_action(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory(package_name), 'launch', 'spawn_robot.launch.py')
+            )
+        )
+    )
+
+    ld.add_action(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory(package_name), 'launch', 'robot_state_publisher.launch.py')
+            )
+        )
+    )
+
+    # ld.add_action(
+    #     IncludeLaunchDescription(
+    #         PythonLaunchDescriptionSource(
+    #             os.path.join(get_package_share_directory('aws-robomaker-small-warehouse-world'), 'launch', 'no_roof_small_warehouse.launch.launch.py')
+    #         )
+    #     )
+    # )
+
+    return ld
--- update.bash
+++ update.bash
@@ -3,10 +3,10 @@
 DIR_NAME="$(dirname $0)"
 
 # Extentions
-rm -rf $DIR_NAME/ext
 rm -rf $DIR_NAME/build $DIR_NAME/install $DIR_NAME/log
 vcs import --recursive < $DIR_NAME/ext.repos
 vcs pull $DIR_NAME/ext
+vcs pull $DIR_NAME/src
 
 sudo apt update && rosdep update
 rosdep install -yi --rosdistro humble --from-paths $DIR_NAME
