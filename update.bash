24faedd57014e43a62f94c88ab8716a759a3bf35
Ayrton2718 <matsushita.sena@gmail.com>
2025-03-06 00:38:19 +0900

    Add 1d-particle-filter as submodule.

--- update.bash
+++ update.bash
@@ -2,13 +2,14 @@
 
 DIR_NAME="$(dirname $0)"
 
+git submodule update --init --recursive
+
 # Extentions
 rm -rf $DIR_NAME/build $DIR_NAME/install $DIR_NAME/log
 vcs import --recursive < $DIR_NAME/ext.repos
 vcs pull $DIR_NAME/ext
-vcs pull $DIR_NAME/src
 
-ansible-playbook --ask-become-pass $DIR_NAME/src/blackbox/ansible/dev.yml 
+ansible-playbook --ask-become-pass $DIR_NAME/ext/blackbox/ansible/dev.yml 
 
 sudo apt update && rosdep update
 rosdep install -yi --rosdistro humble --from-paths $DIR_NAME
